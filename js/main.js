66ee0f345475e5f06488e986e0ec6bf440d5a8fe
kamil_kawa <kamil.kawa@gmail.com>
2016-03-25 22:01:12 +0100

    visuals 90% start of js code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+var generatedList = [];
+var notActiveColors = {1: '#823333', 2: '#065D06' , 3: '#24245F' , 4: '#777719' };
+
 $(document).ready(function(){
+  console.log('click');
+
+  $('#game-blue').on('click', function(){
+
+  });
 
 });
+
+function rand(){
+  return Math.floor(Math.random() * 4 + 1);
+}
+
+function generateNext(generatedList){
+  generatedList.push(rand);
+
+  for(var i = 0; i < generatedList.length; i++){
+
+  }
+}
